77f458d3286da523274c2b451fd95df5ae1552ce
olena4reunited <olenaduringintermission@gmail.com>
2024-09-29 21:18:19 +0300

    build: update urls with router

--- station/urls.py
+++ station/urls.py
@@ -1,28 +1,37 @@
 from django.contrib.admin import action
 from django.urls import path
+from rest_framework import routers
 
 from station.views import BusViewSet
 
 
-bus_list = BusViewSet.as_view(
-    actions={
-        "get": "list",
-        "post": "create"
-    }
-)
+router = routers.DefaultRouter()
 
-bus_detail = BusViewSet.as_view(
-    actions={
-        "get": "retrieve",
-        "put": "update",
-        "patch": "partial_update",
-        "delete": "destroy"
-    }
-)
+router.register("buses", BusViewSet)
 
-urlpatterns = [
-    path("buses/", bus_list, name="bus-list"),
-    path("buses/<int:pk>/", bus_detail, name="bus-detail")
-]
+
+urlpatterns = router.urls
+
+
+# bus_list = BusViewSet.as_view(
+#     actions={
+#         "get": "list",
+#         "post": "create"
+#     }
+# )
+#
+# bus_detail = BusViewSet.as_view(
+#     actions={
+#         "get": "retrieve",
+#         "put": "update",
+#         "patch": "partial_update",
+#         "delete": "destroy"
+#     }
+# )
+#
+# urlpatterns = [
+#     path("buses/", bus_list, name="bus-list"),
+#     path("buses/<int:pk>/", bus_detail, name="bus-detail")
+# ]
 
 app_name = "station"
